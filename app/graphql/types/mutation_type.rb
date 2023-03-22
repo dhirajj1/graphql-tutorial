f44d7e250610bfeb4827fd9814ee40ce9b2a67d9
dhirajj1 <jaiswaldhiraj85@gmail.com>
2023-03-22 20:16:38 +0545

    created Authentication

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,6 +9,7 @@ module Types
   # end
 
   class MutationType < BaseObject
+    field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
   end
 end
